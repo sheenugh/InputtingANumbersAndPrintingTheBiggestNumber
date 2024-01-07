a62e6d2039386d318fba9e6030704d99e4875de2
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-01-07 13:13:20 +0800

    First Commit

---
+++ inputting_numbers_and_printing_the_biggest_number.py
@@ -0,0 +1,13 @@
+#Delima, Sheena Mae D.
+#BSCPE 1-5
+
+#---------------------------------------------
+
+
+# ========== IMPORTS ==========
+# ========== FUNCTIONS ==========
+# ========== ACTUAL CODES =========
+
+# --- Pseudocode ---
+# - Ask user to input 3 numbers
+# - Find and print the biggest number
