0d07ef6a5973010a6476f11e1a6d3bb45aefc6d3
delimasheenamae16@gmail.com <delimasheenamae16@gmail.com>
2024-01-07 23:38:32 +0800

    After Byahe: Windows ni Tkinter

--- inputting_numbers_and_printing_the_biggest_number.py
+++ inputting_numbers_and_printing_the_biggest_number.py
@@ -5,9 +5,25 @@
 
 
 # ========== IMPORTS ==========
+from tkinter import *
+
+
+
 # ========== FUNCTIONS ==========
+
 # ========== ACTUAL CODES =========
 
-# --- Pseudocode ---
+# --- Pseudocode ---_
+# - Tkinter codes window
+class Input_A_Number:
+    def __init__(self, root):
+        pass
+
+root = Tk()
+obj = Input_A_Number(root)
+root.mainloop()
+
+
+
 # - Ask user to input 3 numbers
 # - Find and print the biggest number
